e6a42e975462b3945e1a0efe0e9cc259b2b1d502
Marouane Addou <105064179+marouaneaddou@users.noreply.github.com>
2026-08-30 13:10:43 +0100

    Set up project dependencies

---
+++ src/test/java/com/marouane/book_library_api/BookLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marouane.book_library_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
