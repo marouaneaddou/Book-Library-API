313233d673a93dd3d04381d336b212910e3b2c25
Marouane Addou <105064179+marouaneaddou@users.noreply.github.com>
2026-09-05 13:20:54 +0100

    add global handlers for validation errors

--- src/main/java/com/marouane/book_library_api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/marouane/book_library_api/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.marouane.book_library_api.exceptions;
 
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.http.ResponseEntity;
 import com.marouane.book_library_api.exceptions.ErrorResponse;
 import com.marouane.book_library_api.exceptions.NotFoundException;
@@ -10,6 +11,8 @@ import com.marouane.book_library_api.exceptions.AuthorHasBooksException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import java.time.Instant;
+import java.util.Map;
+import java.util.HashMap;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -42,6 +45,29 @@ public class GlobalExceptionHandler {
                     ex.getMessage(),
                     request.getRequestURI()
                 );
+    }
+
+    @ExceptionHandler( MethodArgumentNotValidException.class )
+    public ResponseEntity<Map<String, Object>> handleConflit( MethodArgumentNotValidException ex, 
+            HttpServletRequest request) {
+            
+            Map<String, String> fieldErrors = new HashMap<>();
+            ex.getBindingResult().getFieldErrors().forEach(error -> {
+                fieldErrors.put(error.getField(), error.getDefaultMessage());
+            });
+
+            Map<String, Object> response = new HashMap<>();
+
+            response.put( "status", HttpStatus.BAD_REQUEST.value() );
+            response.put( "error", HttpStatus.BAD_REQUEST.getReasonPhrase() );
+            response.put( "message", "Validation failed" );
+            response.put( "path", request.getRequestURI() );
+            response.put( "timestamp", Instant.now() );
+            response.put( "fieldErrors", fieldErrors );
+
+            return ResponseEntity
+                .status(  HttpStatus.BAD_REQUEST.value() )
+                .body( response );
     }
 
     private ResponseEntity<ErrorResponse> builderResponse( 
