c5f3c147fb3dcdc7d2b380c824289589906a2032
Marouane Addou <105064179+marouaneaddou@users.noreply.github.com>
2026-09-05 12:47:20 +0100

    add global handlers for application exceptions

--- src/main/java/com/marouane/book_library_api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/marouane/book_library_api/exceptions/GlobalExceptionHandler.java
@@ -5,18 +5,40 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.http.ResponseEntity;
 import com.marouane.book_library_api.exceptions.ErrorResponse;
 import com.marouane.book_library_api.exceptions.NotFoundException;
-import java.lang.RuntimeException;
+import com.marouane.book_library_api.exceptions.ConflictException;
+import com.marouane.book_library_api.exceptions.AuthorHasBooksException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import java.time.Instant;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler( NotFoundException.class )
-    public ResponseEntity<ErrorResponse> handleNotFound( RuntimeException ex, 
+    public ResponseEntity<ErrorResponse> handleNotFound( NotFoundException ex, 
             HttpServletRequest request) {
                 return this.builderResponse( 
                     HttpStatus.NOT_FOUND,
+                    ex.getMessage(),
+                    request.getRequestURI()
+                );
+    }
+
+    @ExceptionHandler( ConflictException.class )
+    public ResponseEntity<ErrorResponse> handleConflit( ConflictException ex, 
+            HttpServletRequest request) {
+                return this.builderResponse( 
+                    HttpStatus.CONFLICT,
+                    ex.getMessage(),
+                    request.getRequestURI()
+                );
+    }
+
+    @ExceptionHandler( AuthorHasBooksException.class )
+    public ResponseEntity<ErrorResponse> handleConflit( AuthorHasBooksException ex, 
+            HttpServletRequest request) {
+                return this.builderResponse( 
+                    HttpStatus.CONFLICT,
                     ex.getMessage(),
                     request.getRequestURI()
                 );
